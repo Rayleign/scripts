0db023b47aa7616ace811e510b5ce1ef4f4005ad
Rayleign <rayleign5@163.com>
2017-02-11 15:23:17 +0800

    add iterm

--- mac_app_launch/run.rb
+++ mac_app_launch/run.rb
@@ -14,10 +14,11 @@ end
 path= File.dirname(__FILE__)
 
 if app=='all'
-    s=`cd #{path} && ls *.scpt|grep -v commom`.split("\n")
+    s=`cd #{path} && ls *.scpt|grep -v commom|grep -v foxmail|grep -v iterm`.split("\n")
     for scrp in s
         system "osascript #{path}/#{scrp} #{action}"
     end
+    system "osascript #{path}/iterm.scpt #{action}"
 else
     system "osascript #{path}/#{app}.scpt #{action}"
 end 
